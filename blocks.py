1e653e90cbb6cf06992f1baeacc090b5e7e7f956
shennian <shen.n1993@gmail.com>
2015-04-16 16:53:48 +0800

    v0.0.2

--- blocks.py
+++ blocks.py
@@ -1,51 +1,143 @@
+# coding=utf-8
 import pygame
 from pygame.locals import *
 from sys import exit
 import time
+import random
 
-pygame.init()
-screen = pygame.display.set_mode((900, 600), 0, 32)
-screen.fill((255,255,255))
-pygame.display.set_caption("Blocks")
 
-bar = Rect((0,570),(100,30))
+class Game():
+    def __init__(self):
+        self.colors = self.color_set()
+        self.screen = self.init_screen()
+        # 初始化bar和x,y坐标
+        self.bar_x = 400
+        self.bar_y = 570
+        self.bar = Rect(400, 570, 100, 30)
+        # 初始化block和x,y坐标
+        self.block_x = 440
+        self.block_y = 550
+        # block的移动速度，
+        self.speed_x = 0
+        self.speed_y = 0
+        self.block = Rect(440, 550, 20, 20)
+        # 目标砖块，用二维数组存储，坐标值
+        self.targets = self.init_targets()
+        # 将目标砖块的二维数组draw在屏幕上
+        self.display_targets()
+        # 帧数控制
+        self.times = 0
+        # 游戏开始控制
+        self.game_start = 0
+        
+    def init_screen(self):
+        screen = pygame.display.set_mode((900, 600), 0, 32)
+        screen.fill(self.colors[0])
+        pygame.display.set_caption("Blocks")
+        return screen
+    
+    def init_targets(self):
+        targets = [[0 for j in range(9)] for i in range(20)]
+        for target_num in range(18):
+        	x = random.randint(0,8)
+        	y = random.randint(0,11)
+        	targets[y][x] = 1
+        return targets
+    
+    # 设置下一时刻block的坐标
+    def set_block_position(self):
+        _x = self.block_x
+        _y = self.block_y  	
+        _x += self.speed_x
+        _y += self.speed_y       
+        if _x <= 0 or _x >= 880:
+            self.speed_x = -self.speed_x
+        elif _y <= 0:
+            self.speed_y = -self.speed_y
+        elif _y >= 550:
+            if self.block_x > self.bar_x and self.block_x < self.bar_x+100:
+                self.speed_y = -self.speed_y
+        # 将block的坐标映射在targets的二维数字上，如果值为1，表示发生碰撞
+        elif self.targets[(_y+10)/30 % 20][(_x+10)/100 % 9] == 1:
+            self.targets[(_y+10)/30 % 20][(_x+10)/100 % 9] = 0
+            # block的状态转移
+            if self.speed_x > 0 and self.speed_y > 0:
+                self.speed_y = -self.speed_y
+            elif self.speed_x > 0 and self.speed_y < 0:
+                self.speed_y = -self.speed_y
+            elif self.speed_x < 0 and self.speed_y < 0:
+    	        self.speed_y = -self.speed_y
+    	    elif self.speed_x < 0 and self.speed_y > 0:
+    	        self.speed_y = -self.speed_y
+            
+    def block_move(self):    
+    	time.sleep(1/60)	    
+    	if self.times == 60:
+           self.times = 0
+        if self.times % 5 == 0:
+            self.set_block_position()
+            self.block.move_ip(self.speed_x, self.speed_y)
+            self.block_x += self.speed_x
+            self.block_y += self.speed_y    
+        self.times += 1
+        
+    def display_targets(self):
+        for i in range(20):
+           for j in range(9):
+               if self.targets[i][j] == 1:
+                   t = Rect(j*100, i*30, 100, 30)
+                   num = random.randint(1,6)
+                   pygame.draw.rect(self.screen, self.colors[num], t)
+    
+    # 初始颜色
+    def color_set(self):
+    	colors = []
+    	white = (255, 255, 255)
+    	colors.append(white)
+    	colors.append((0, 255, 255))
+    	colors.append((255, 0, 0))
+    	colors.append((255, 0, 255))
+    	colors.append((255, 127, 0))
+    	colors.append((255, 0, 127))
+    	colors.append((95, 0, 191))
+    	colors.append((0, 0, 0))
+    	return colors
+    	  
+    def play(self):
+        while True:
+            self.block_move()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()   
+                elif event.type == KEYDOWN:
+                    if event.key == K_LEFT:
+                        if self.bar_x > 0:
+                            self.bar.move_ip(-20,0)
+                            self.bar_x -= 20
+                        if self.game_start == 0:
+                            self.block.move_ip(-20,0)
+                            self.block_x -= 20
+                        
+                    elif event.key == K_RIGHT:
+                        if self.bar_x < 800:
+                            self.bar.move_ip(20,0)
+                            self.bar_x += 20
+                        if self.game_start == 0:
+                            self.block.move_ip(20,0)
+                            self.block_x += 20
+                    elif event.key == K_SPACE:
+                        if self.game_start == 0:
+                            self.game_start = 1
+                            self.speed_x = 1
+                            self.speed_y = -1           
+            pygame.draw.rect(self.screen, self.colors[7], self.bar)
+            pygame.draw.rect(self.screen, self.colors[2], self.block)
+            self.display_targets()
+            pygame.display.flip()
+            self.screen.fill((255,255,255))
+    
+if __name__ == "__main__":
+    test = Game()
+    test.play()   
 
-position = [21, 21]
-dx = 20
-dy = 20
-block = Rect((position[0], position[1]),(20,20))
-
-targets = []
-for i in range(10):
-	pass
-def block_move():
-	global dx, dy, position
-	block.move_ip(dx, dy)
-	position[0] += dx
-	position[1] += dy
-	if position[1] >= 550 or position[1] <= 20:
-		print "yes"
-		dy = -dy
-	if position[0] >= 880 or position[0] <= 20:
-		dx = -dx
-	
-	
-		
-while True:
-	time.sleep(0.2)
-	block_move()
-	
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			exit()
-		if event.type == KEYDOWN:
-			if event.key == K_LEFT:
-				bar.move_ip(-30,0)
-			if event.key == K_RIGHT:
-				bar.move_ip(30,0)
-			
-		
-	pygame.draw.rect(screen, (0,0,0), bar)
-	pygame.draw.rect(screen, (0,0,0), block)
-	pygame.display.flip()
-	screen.fill((255,255,255))
+                
